ad07e2c32a23b6bb9588874dad6c69fe81ec012f
Anych <anuar123@mail.ru>
2022-08-23 00:20:38 +0600

    Pass all tests

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -2,6 +2,57 @@ package com.epam.rd.autotasks;
 
 class Spiral {
     static int[][] spiral(int rows, int columns) {
-        return null;
+        int[] arr = new int[rows * columns];
+        int count = 1;
+        for (int i = 0; i < rows * columns; i++) {
+            arr[i] = count++;
+        }
+
+        int[][] mat = new int[rows][columns];
+
+        int top = 0, bottom = rows - 1;
+        int left = 0, right = columns - 1;
+
+        int index = 0;
+
+        while (true)
+        {
+            if (left > right) {
+                break;
+            }
+
+            for (int i = left; i <= right; i++) {
+                mat[top][i] = arr[index++];
+            }
+            top++;
+
+            if (top > bottom) {
+                break;
+            }
+
+            for (int i = top; i <= bottom; i++) {
+                mat[i][right] = arr[index++];
+            }
+            right--;
+
+            if (left > right) {
+                break;
+            }
+
+            for (int i = right; i >= left; i--) {
+                mat[bottom][i] = arr[index++];
+            }
+            bottom--;
+
+            if (top > bottom) {
+                break;
+            }
+
+            for (int i = bottom; i >= top; i--) {
+                mat[i][left] = arr[index++];
+            }
+            left++;
+        }
+        return mat;
     }
 }
